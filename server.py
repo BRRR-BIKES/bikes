a0426946423aa5113f84f8cc7695962f58a8b9be
BRRR-BIKES <44409179+BRRR-BIKES@users.noreply.github.com>
2018-10-23 18:46:22 +0200

    Update server.py

--- server.py
+++ server.py
@@ -87,7 +87,7 @@ class S(BaseHTTPRequestHandler):
     def do_POST(self):
         self._set_headers()
 
-def run(server_class=HTTPServer, handler_class=S, port=80):
+def run(server_class=HTTPServer, handler_class=S, port=8080):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print 'Starting httpd...'
